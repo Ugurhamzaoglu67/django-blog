fe5a4e3604540573c9a6c683218c8026ae0b261a
hmzugur <craxx67@gmail.com>
2021-03-04 09:06:12 +0300

    kategori ve arama ayarları yapıldı..

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from blog.views import iletisim, anasayfa, kategori #kategori view'ında çağırdık
+from blog.views import iletisim, anasayfa, kategori, yazilarim #kategori view'ında çağırdık
 
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
     path('iletisim', iletisim, name='iletisim'),
-    path('kategori/<slug:kategoriSlug>', kategori, name='kategori')
+    path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
+    path('yazilarim',yazilarim, name='yazilarim')
 ]
 
 
--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from blog.models  import YazilarModel
 from django.core.paginator import Paginator
+from django.db.models import Q # ya da işlemi için
+
+
 
 def anasayfa(request):
+      sorgu = request.GET.get('sorgu')
+      
       yazilar = YazilarModel.objects.order_by('-id') #en son yazı
+      if sorgu:
+            yazilar = yazilar.filter(
+                  Q(baslik__icontains = sorgu) | 
+                  Q(icerik__icontains = sorgu)
+                  ).distinct()
+
       sayfa=request.GET.get('sayfa')
       paginator = Paginator(yazilar,2) #Bir sayafada kaç kayıt olsun -> 2
       
